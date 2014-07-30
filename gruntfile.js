aee9f2e3e1ab4522250be847b0e1bde198f1f3fc
irwinna <nathan.irwin@vuw.ac.nz>
2014-07-30 12:53:40 +1200

    Grunt update for two style sheets

--- gruntfile.js
+++ gruntfile.js
@@ -39,14 +39,59 @@ module.exports = function(grunt) {
             },
             sass: {
                 files: "**/*.scss",
-                tasks: ["sass:dev"],
+                tasks: ["sass:dev", "csssplit:dev"],
             },
             all: {
                 files: "**/*",
             },
         },
+        csssplit: {
+            dev: {
+              src: 'www/assets/css/style.css',
+              dest: 'www/assets/css/',
+              options: {
+                  maxRules: 2000,
+                  maxPages: 3,
+                  suffix: ''
+              },
+            },
+            prod: {
+              src: 'www/assets/css/style.css',
+              dest: 'www/assets/css/grunt_css/',
+              options: {
+                  maxRules: 1000,
+                  maxPages: 3,
+                  suffix: ''
+              },
+            }
+        },
+        cssmin: {
+          combine: {
+            files: {
+              'www/assets/css/style1.css': ['www/assets/css/grunt_css/style1.css'],
+              'www/assets/css/style2.css': ['www/assets/css/grunt_css/style2.css'],
+            }
+          }
+        },
     });
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-livereload');
+    grunt.loadNpmTasks('grunt-csssplit');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+
+    grunt.registerTask('dev', 'the development state task', function() {
+      // Enqueue "bar" and "baz" tasks, to run after "foo" finishes, in-order.
+      grunt.task.run('sass:dev', 'csssplit:dev');
+    });
+
+    grunt.registerTask('prod', 'the production ready task', function() {
+      grunt.task.run('sass:prod', 'csssplit:prod', 'cssmin');
+    });
+
+
+    grunt.registerTask('default', 'My "default" task description.', function() {
+      grunt.log.writeln('grunty: Here is what you can do! grunt dev - this task complies the sass with line numbers and split the style.css into two sheets. grunt prod -  this task complies the sass, splits the style.css into two sheets then minifys the new sheets. grunt watch - this will watch the folders for changes run grunt dev if sass has changed and it auto reloads your browser. Boom! grunt on!');
+    });
 };
