885238466422591ae92fa2f326ab816584ba368a
dickieirwin <nathan.irwin@outlook.com>
2014-10-01 12:15:12 +1300

    Single hero styles for positioning text

--- www/inc/footer_new.php
+++ www/inc/footer_new.php
@@ -1,69 +1,9 @@
             </div> <!-- end #page -->
-		
-		<?php 
-			if (isset($homepage)) {
-				echo "<div class='connect_wrapper'> 
-                        <div class='feeds_wrap'>
-                        <aside class='connect-feeds'>
-                                                        <article class='connect_intro'>
-                                                            <h2>Stay in touch&hellip;</h2>
-                                                            <p>Connect with Victoria by following us on <a href='http://www.facebook.com/victoriauniversityofwellington'>Facebook</a>, <a href='http://twitter.com/VicUniWgtn'>Twitter</a> and <a href='http://www.linkedin.com/company/3693'>LinkedIn</a> or subscribe to our <a href='http://www.youtube.com/user/victoriamarketing'>YouTube channel</a>.</p>
-                                                        </article>
-                                                        
-                                                        <article class='feed'>
-                                                            <h3>&hellip;on Facebook</h3>
-                                                            <header class='feed__header '>
-                                                                <a href='#'>
-                                                                    <span class='icon facebook'></span>
-                                                                    Victoria University of Wellington
-                                                                </a>
-                                                                <div class='feed__button channel__button'>
-                                                                    <div class='fb-like' data-href='https://www.facebook.com/victoriauniversityofwellington' data-layout='button_count' data-show-faces='true' data-width='450'></div>
-                                                                </div>
-                        
-                                                            </header>
-                                                            <a href='#'><p class='feed__body'>We've been talking to some of our students about their experience at Victoria, and we’re excited to share their stories with you over the next few weeks. Watch this...</p></a>
-                                                            <footer class='feed__footer'>
-                                                            <time>31 Jan</time>  
-                                                            <span class='actions'>
-                                                                <a href='#' class='action'><span class='icon share'></span>Share</a>
-                                                            </span>
-                                                            </footer>
-                                                        </article>
-                                                        
-                                                        <article class='feed'>
-                                                            <h3>&hellip;on Twitter</h3>
-                                                            <header class='feed__header '>
-                                                                <a href='#'>
-                                                                    <span class='icon twitter'></span>
-                                                                    Victoria University <b>@VicUniWgtn</b>
-                                                                </a>
-                        
-                                                                <div class='feed__button channel__button'>
-                                                                    <a href='https://twitter.com/VicUniWgtn' class='twitter-follow-button' data-show-count='false' data-dnt='true'>Follow @VicUniWgtn</a>
-                        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-                                                                </div>
-                        
-                                                            </header>
-                                                            <p class='feed__body'>Tonight we celebrate our distinguished alumni. Congrats to the six outstanding <a href=''>@VicUniWgtn</a> alumni receiving awards! <a href=''>http://bit.ly/13xCvN2</a></p>
-                                                            <footer class='feed__footer'>
-                                                                 <time>31 Jan</time>  
-                                                                 <span class='actions'>
-                                                                     <a href='#' cdivass='action'><span class='icon fave'></span>Favourite</a> 
-                                                                     <a href='#' class='action'><span class='icon retweet'></span>Retweet</a> 
-                                                                     <a href='#' class='action'><span class='icon reply'></span>Reply</a>
-                                                                 <span>
-                                                            </footer>
-                                                        </article>
-                                                    </aside></p>
-                                                </div>
-</div>";	
-                  
-			}
-		?>
-			      
+
+
+
         <footer role='contentinfo' class='site_footer'>
-            <div class="site_links wrap">             
+            <div class="site_links wrap">
                 <div class="cols">
                     <h2><a href="./?a=119780">Contacts and directories</a></h2>
                       <ul class="link_list">
@@ -80,9 +20,9 @@
                       <li><a href="./?a=147087">Karori Campus</a></li>
                       <li><a href="./?a=147083">Te Aro Campus</a></li>
                     </ul>
-                  
-                    
-                    
+
+
+
                 </div>
                 <div class="cols">
                     <h2><a href="./?a=147146">Faculties</a></h2>
@@ -98,8 +38,8 @@
                       <li><a href="./?a=53831">Victoria Business School</a></li>
                     </ul>
 
-                  
-                  
+
+
                 </div>
                 <div class="cols">
                     <h2><a href="./?a=113805">Future students</a></h2>
@@ -116,15 +56,15 @@
                     <li><a href="./?a=118997">Services and support</a></li>
                     <li><a href="./?a=119313">Financial survival</a></li>
                     <li><a href="./?a=119079">Key dates</a></li>
-                    
+
                   </ul>
                 </div>
-                <div class="cols">  
+                <div class="cols">
                   <h2><a href="">Tools</a></h2>
                   <ul class="link_list">
                     <li><a href="https://webmail.vuw.ac.nz">Staff email</a></li>
                     <li><a href="http://outlook.com/myvuw.ac.nz">Student email</a></li>
-                    <li><a href="https://my.vuw.ac.nz/">MyVictoria</a></li> 
+                    <li><a href="https://my.vuw.ac.nz/">MyVictoria</a></li>
                     <li><a href="http://library.victoria.ac.nz/library/">Library</a></li>
                     <li><a href="http://blackboard.vuw.ac.nz/">Blackboard</a></li>
                   </ul>
@@ -157,14 +97,23 @@
                 <div class="fineprint wrap">
 
                             <div class="contact_info">
-                              <img class="footer_logo" src="assets/images/vic-logo-mobile.png">
-                               
+                              <div class="footer_logo">
+                             
+                              <h2 class="word_logo"><a href="http://victoria.ac.nz">VICTORIA</a></h2>
+                              </div> 
                               <ul class="details">
-                                  <h2><a href="./?a=119780">Contact Us</a></h2>
+                                 
+                                  <!-- <li><h2 class="word_logo"><a href="http://victoria.ac.nz">VICTORIA</a></h2></li> -->
                                   <li><a href='tel:+6444721000'>+64 4 472 1000</a></li>
-                                  <li>For enrolment enquiries: <a href='tel:+64800842867'>0800 VICTORIA</a></li>
-                                  <li>In an emergency: ext. 8888 (internal) or <a href='tel:+6444639999'>+64 4 463 9999</a> (external)</li>          
+                                  <li><a href='mailto:info@victoria.ac.nz'>info@victoria.ac.nz</a></li>
+                                  <li>Enrolments: <a href='tel:+64800842867'>0800 VICTORIA</a></li>
+                                  <li>Emergency: <a href='tel:+6444639999'>+64 4 463 9999</a> or ext. 8888 (internal)</li>
+
                               </ul>
+                                
+                              
+
+                              
                             </div>
                             <hr class="hiding">
                             <nav>
@@ -182,12 +131,26 @@
         </footer>
     </div><!-- /wrapper -->
     <style type="text/css">
+        
+         .site_footer .contact_info h2.word_logo {
 
-        .footer_logo {
-          width: 140px;
-          float: left;
-          padding: 0 3% 0 0;
+          text-transform: uppercase;
+          font-family: "Nimbus Roman No 9 L","FreeSerif","Georgia",serif;
+          font-weight: 600;
+          font-size: 1.5em;
+          color: #bbb;
+          margin-right: 1em;
         }
+
+        /*.footer_logo {
+          height: 50px;
+            margin:  0;
+            background-image:  url('/assets/images/logo_desktop.png');
+            background-size: 160px;
+            background-position: 0 50%;
+            background-repeat: no-repeat;
+            padding-left: 180px;
+        }*/
 
         .homepage .site_footer {
             padding: 0;
@@ -200,7 +163,7 @@
         }
 
         .site_footer .contact_info {
-            padding-bottom: .25em;
+            padding-bottom: 0;
             padding-top: 0;
             margin-bottom: 0;
             margin-left: auto;
@@ -211,23 +174,24 @@
         .fineprint.wrap {
             border-top: 0;
         }
-        
+
         .site_footer .contact_info h2 {
             text-transform: uppercase;
             font-family: "National","Segoe WP Semibold","Nimbus Sans L",Arial,"Helvetica Neue","Helvetica",sans-serif;
             font-weight: 600;
             font-size: 1.5em;
         }
-        
+
         .site_footer .details {
-            line-height: 25px;
+            line-height: 1.5;
             font-size: 1.15em;
+            padding-top: 1px;
         }
-        
+
         .site_footer .fineprint hr {
             height: 1px;
             background-color: #999;
-            margin: 0 0 1em;
+            margin: 0.5em 0;
         }
         .site_footer .details li a {
             color: #fff;
@@ -249,6 +213,20 @@
               display: none;
             }
 
+          .details {
+            margin-top: 1em;
+          }
+
+          .connect_wrapper {
+            margin-top: 0em;
+          }
+
+          .site_footer {
+            padding-left: 0;
+            padding-right: 0;
+          }
+
+      
          @media all and (max-width : 1140px) /*aka @wide*/ {
 
             .site_links.wrap {
@@ -262,11 +240,24 @@
             }
            .footer_logo {
               margin: 1em 0;
-            } 
+            }
          }
 
          @media all and (max-width: 759px) {
-            
+
+
+
+           /* .footer_logo {
+                 background: url(/assets/images/vic-logo-mobile.png) no-repeat;
+            background-size: 128px 17px;
+            background-position: 50% 50%;
+            width: 100%;
+            height: 30px;
+            float: none;
+            padding: 0;
+            margin: 0;
+            }*/
+  
              .site_footer .contact_info {
                 display: block;
              }
@@ -278,7 +269,7 @@
             .site_footer .fineprint nav a {
               color: #999;
             }
-            
+
             .site_footer .fineprint hr.hiding {
               display: none;
             }
@@ -309,8 +300,8 @@
     </style>
 	<script src='/assets/js/common.js'></script>
     <script src='/assets/js/search.js'></script>
-    
-	
+
+
     </body>
 
 </html>
